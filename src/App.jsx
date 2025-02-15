63dc144e8c4b192ce60e31e1fbace2ce1b4e02c6
Bagadi Bharat <bharatbagadi923@gmail.com>
2025-02-15 16:48:54 +0530

    Completed the About us part of the website apart for some tweaks in responsiveness, it will be done in the upcoming pr's

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Team from './components/Team'
 import Verticles from './components/Verticles'
 import './App.css'
+import About from './components/About'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/home' element={<Home />} />
         <Route path='/team' element={<Team />} />
         <Route path='/verticles' element={<Verticles />} />
+        <Route path='/aboutus' element={<About/>}/>
       </Routes>
     </>
   )
